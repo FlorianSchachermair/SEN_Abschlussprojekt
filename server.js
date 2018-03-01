ffc7e636b0d0535707c86392009cd66773d08523
FlorianSchachermair <florianschachermair@htl-braunau.at>
2018-03-01 11:14:19 +0100

    server changes

--- server.js
+++ server.js
@@ -17,7 +17,10 @@ connection.connect()
 
 let newClass = {year:5, label:'AHELS'}
 let newStudent = {firstname:'Florian', lastname:'Schachermair', year:5, classLabel:'AHELS'}
+let newSubject = {label:'FSST'}
+let newTest = {subject:'FSST', classLabel:'AHELS', year:5, date:'2017-03-01'}
 
+addSubject(newSubject)
 function addClass(addClass){
     let sqlQuery = 'insert into Klassen(Jahrgang, Bezeichnung) values(' + addClass.year + ', "' + addClass.label + '");'
     connection.query(sqlQuery, function(error, results, fields){
@@ -43,9 +46,12 @@ function addStudent(student){
     })
 }
 function addTest(test){
-    let sqlQuery = 'insert into Tests(Fach, Datum, KID) values("' + student.firstname + '", "' + student.lastname + '", '
-    sqlQuery+= '(select KID from Klassen where Bezeichnung="' + student.classLabel + '" ' 
-    sqlQuery+= 'and Jahrgang=' + student.year +'));'
+    let sqlQuery = 'insert into Tests(FID, KID, Datum) values('
+    sqlQuery+='(select FID from Faecher where Bezeichnung ="' + test.subject + '"),'
+    sqlQuery+= '(select KID from Klassen where Bezeichnung="' + test.classLabel + '" ' 
+    sqlQuery+= 'and Jahrgang=' + student.year +'),'
+    sqlQuery+= test.date + ');'
+    console.log(sqlQuery)
     connection.query(sqlQuery, function(error, results, fields){
             if(error){
                 console.log(error)
@@ -55,7 +61,17 @@ function addTest(test){
             }
     })
 }
-
+function addSubject(newSubject){
+    let sqlQuery = 'insert into Faecher(Bezeichnung) values("' + newSubject.label + '");'
+    connection.query(sqlQuery, function(error, results, fields){
+            if(error){
+                console.log(error)
+                return error
+            }else{
+                return true
+            }
+    })
+}
 
 
 
